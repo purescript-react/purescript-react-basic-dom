3105e5f23829a73cdc6e28f1f388c5776fe3d14e
Matt Russell <matthewjosephrussell@gmail.com>
2022-06-17 21:13:58 -0700

    Add basic test and fix import issue with react server dom (#39)

--- src/React/Basic/DOM/Server.js
+++ src/React/Basic/DOM/Server.js
@@ -1,3 +1,3 @@
-import ReactDOMServer from "react-dom/server";
+import ReactDOMServer from "react-dom/server.js";
 export var renderToString = ReactDOMServer.renderToString;
 export var renderToStaticMarkup = ReactDOMServer.renderToStaticMarkup;
