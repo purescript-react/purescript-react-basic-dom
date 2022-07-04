8a209d0932220eedbe2ec86ebb3b33d919d97d1d
sigma-andex <77549848+sigma-andex@users.noreply.github.com>
2022-07-04 09:16:14 +0100

    Add Maybe instance

--- src/React/Basic/DOM/Simplified/ToJSX.purs
+++ src/React/Basic/DOM/Simplified/ToJSX.purs
@@ -6,6 +6,7 @@ module React.Basic.DOM.Simplified.ToJSX
 import Prelude
 
 import Data.Array (singleton)
+import Data.Maybe (Maybe(..))
 import React.Basic (JSX)
 import Unsafe.Coerce (unsafeCoerce)
 
@@ -16,6 +17,9 @@ instance ToJSX (Array JSX) where
   toJSX = identity
 else instance (ToJSX t) => ToJSX (Array t) where
   toJSX arr = arr >>= toJSX
+else instance (ToJSX t) => ToJSX (Maybe t) where
+  toJSX (Just jsx) = toJSX jsx
+  toJSX Nothing = []
 else instance ToJSX String where
   toJSX = unsafeCoerce >>> singleton
 else instance ToJSX JSX where
